eab488aac90d45d2f485f3eec1242706a223ebd3
Max Base <MaxBaseCode@Gmail.Com>
2021-09-03 12:34:24 +0000

    Update tag-engine.js

--- tag-engine.js
+++ tag-engine.js
@@ -1,5 +1,11 @@
-const tag_engine = (input, tags={}) => {
+// Max Base
+// https://github.com/BaseMax/TinyTagEngineJS
+
+const tag_engine = (input, tags) => {
+    if(tags === undefined || tags === []) tags = {};
+
     let res = input;
+
     for( let tag in tags) {
         const reg = new RegExp(`/\[(\s*|)${tag}(\s*|)(\:(\s*|)([0-9]+)(\s*|)|)\]/g`)
         res = res.replace(reg, function(e) {
@@ -10,5 +16,6 @@ const tag_engine = (input, tags={}) => {
             return tags[tag](id[4]);
         });
     }
+
     return res;
 };
