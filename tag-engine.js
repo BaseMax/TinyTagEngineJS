51406ad5ac91ebeee9d6d5aa550865fe3bd5c453
Max Base <MaxBaseCode@gmail.com>
2021-09-04 23:46:48 +0430

    Fix the bug inside the pattern REGEX

--- tag-engine.js
+++ tag-engine.js
@@ -1,5 +1,8 @@
-// Max Base
-// https://github.com/BaseMax/TinyTagEngineJS
+/*
+ * @Author: Max Base
+ * @Date: 2021-08-31, 2021-09-04
+ * Repository: https://github.com/BaseMax/TinyTagEngineJS
+ */
 
 const tag_engine = (input, tags) => {
     if(tags === undefined || tags === []) tags = {};
@@ -7,9 +10,9 @@ const tag_engine = (input, tags) => {
     let res = input;
 
     for( let tag in tags) {
-        const pattern = `/\\[(\s*|)${tag}(\\s*|)(\\:(\\s*|)([0-9]+)(\\s*|)|)\\]/g`;
+        const pattern = `\\[(\\s*|)${tag}(\\s*|)(\\:([a-zA-Z0-9]+)|)\\]`;
         console.log(tag, pattern);
-        const reg = new RegExp(pattern);
+        const reg = new RegExp(pattern, 'g');
         res = res.replace(reg, function(e) {
             const id = e.match(reg);
             if(!id || id[4] === undefined) {
